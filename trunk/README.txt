25266b2b73a8af942c034480272b1307ae88e090
bhawmik <bhawmik@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-09-08 23:21:15 +0000

    tagging version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/webcollage/tags/1.0@63629 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/README.txt
@@ -0,0 +1,38 @@
+=== Web Collage ===
+Contributors: Sudipta Bhawmik
+Donate link: 
+Tags: links, blogroll, screen shot, artviper
+Requires at least: 2.0.2
+Tested up to: 2.5
+Stable tag: 1.0
+
+
+WebCollage is a cute plugin for Wordpress sites. It creates a collage of snapshots of the websites that you have in your 
+
+links database or blogroll.
+
+== Description ==
+
+Webcollage creates a collage of snapshots of the webpages of your links (blogroll) on any page in your Wordpress site. All 
+
+you need to do is specify the tag "[webcollage]" in the page where you want the collage to appear. 
+Click on any snapshot and it will take you to the corresponding website. If you have the "Snapshots" plugin installed, you 
+
+can see a larger screen image of the website when you bring your mouse pointer over any image.
+
+The beauty of the collage depends on the websites you link to.
+Webcollage uses artviper.net's screen shot generation service. This service is free as long as the usage is non-commercial. 
+
+Hence, please do not use this plugin on any commercial web site.
+The website snapshots may take few minutes to load up. But once they are in your cache, the collage gets generated very 
+
+quickly.
+
+== Installation ==
+
+Download the zip file and follow the instructions below:
+
+    * Unzip the file
+    * Upload the folder webcollage to your wordpress plugins directory/folder
+    * Activate the plugin in your wordpress admin/plugins page.
+    * To create a webcollage on a page, insert the tag "[wecollage]".
---
+++ trunk/webcollage.php
@@ -0,0 +1,79 @@
+<?php 
+/* 
+Plugin Name: WebCollage 
+Plugin URI: http://www.bhawmik.com/
+Description: Plugin for displaying a collage of website images with links
+Version: 1.0 
+Author: Sudipta Bhawmik
+Author URI: http://www.bhawmik.com 
+Disclaimer: Use at your own risk. No warranty expressed or implied is provided.
+
+Copyright 2008 Sudipta Bhawmik (email: bhawmik@gmail.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+function webcollage_function ($text)
+{
+  global $wpdb; 
+  // Edit the next line to set the default maximum number of Rows
+  $MAX_NO_POSTS = 5;
+  
+  
+  $weblinks = get_bookmarks();
+  
+//	$text = str_replace('[/webcollage]','</h2>',$text);  // no option
+
+  
+  $query = ""; 
+  $feedURL = $weblinks;
+  $iFeeds = count($feedURL);  // Find out how many feeds on the page
+//  echo "Number of Links = ". $iFeeds; 
+  $atext = "<table><tr>"; // reset replacement for string between  brackets
+  $link_urls = $wpdb->get_col($query, 1);
+  $j = 0;
+  for ($i = 0; $i < $iFeeds; $i++)
+  {
+    
+//    $link = get_bookmark_field('link_url',  $i, 'display');
+//    echo $link;
+       if ($i == 3) {  
+	       $atext .= "<td><a href=\"http://nynjbengali.ethnomediallc.com\"><img src=\"http://www.artviper.net/screenshots/screener.php?url=$link_urls[$i]&w=90\"></a></td>"; 
+       }
+       else {
+	       $atext .= "<td><a href=\"$link_urls[$i]\"><img src=\"http://www.artviper.net/screenshots/screener.php?url=$link_urls[$i]&w=90\"></a></td>";
+       }
+    if ($j == 4)
+      {
+       $j = 0;
+       $atext .= "</tr><tr>";
+      }
+   else
+       $j++; 
+   } 
+    // this finds the particular feed between the feedsnap brackets and replaces
+    // it with the assembled feed text
+  $atext .= "</tr></table>";
+  
+  $text = str_replace('[webcollage]', $atext, $text); //allow for option
+  	
+  return $text;
+}
+
+
+
+add_filter('the_content','webcollage_function');
+
+?>
